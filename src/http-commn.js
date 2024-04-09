c0769752491126035c4d61674fa87ef80e3f87a9
HaDzE7 <hadebayaa@gmail.com>
2024-04-10 01:10:56 +0300

    bugged fixed

--- src/http-commn.js
+++ src/http-commn.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+let baseUrl = window.location.origin;
+if (baseUrl.includes("localhost")) {
+  baseUrl = "https://volunteers-1.onrender.com";
+}
 const http = axios.create({
   // baseURL: "https://volunteers.onrender.com/",
-  baseURL: "http://localhost:5000/",
+  // baseURL: "https://volunteers-1.onrender.com",
+  baseURL: baseUrl,
 
   headers: {
     "Content-type": "application/json",
